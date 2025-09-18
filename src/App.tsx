86d2bcd041499867f73dc43dd5ea76cb79150cdb
ikz87 <iikz87ii@gmail.com>
2025-09-18 16:07:54 -0600

    feat: some progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import { HardShadow } from './components/ui/HardShadow';
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { HardShadowButton } from './components/ui/HardShadowButton.tsx';
+import { HardShadowRect } from './components/ui/HardShadowRect.tsx';
+import { Home } from 'lucide-react';
 
 function App() {
-
   return (
-    <HardShadow>
-      <div className="bg-red-50 p-2">
-        some content
+    <>
+      <div className="h-dvh w-dvw cyan-100 p-8">
+        <div className="w-full flex">
+          <HardShadowButton className="bg-rose-200 p-4" rotate={0}>
+            <Link to="/">
+              <Home className="" strokeWidth={4}/>
+            </Link>
+          </HardShadowButton>
+        </div>
       </div>
-    </HardShadow>
+    </>
   )
 }
 
